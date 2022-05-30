ed5c36d47fc696ab81a4900b77147dc465858da6
shu786ash123 <106545357+shu786ash123@users.noreply.github.com>
2022-05-30 22:02:41 +0530

    Create 30.py

---
+++ 30.py
@@ -0,0 +1,8 @@
+# 17.Write a Python NumPy program to compute the weighted 
+# average along the specified axis of a given flattened array.
+import numpy as np
+a = np.arange(9).reshape((3,3))
+print("Original flattened array:")
+print(a)
+print("Weighted average along the specified axis of the above flattened array:")
+print(np.average(a, axis=1, weights=[1./4, 2./4, 2./4]))
